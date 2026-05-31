f9833941a0129143f38ff421aee2e609f020d86b
liuxin85 <liuxin85@gmail.com>
2026-05-31 17:17:25 +0800

    Including Proto Files in the dist Directory

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -6,16 +6,24 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { PRODUCTS_PACKAGE_NAME } from '../../../types/proto/products';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: PRODUCTS_PACKAGE_NAME,
+        protoPath: join(__dirname, 'proto/products.proto'),
+      },
+    },
   );
+  await app.listen();
+  Logger.log(`🚀 Application is running on gRPC channel`);
 }
 
 bootstrap();
--- note.md
+++ note.md
@@ -7,9 +7,31 @@ nx g @nx/nest:app apps/products
 npm i @nestjs/microservices
 
 ### For grpc
+
 npm i @grpc/grpc-js @grpc/proto-loader
 
 ### Create products.proto in proto directory and generate ts file
+
 npm i ts-proto
 
-npx protoc --ts_proto_out=./types/ ./proto/*.proto --ts_proto_opt=nestJs=true
+npx protoc --ts_proto_out=./types/ ./proto/\*.proto --ts_proto_opt=nestJs=true
+
+nx run-many -t serve --all
+
+### fix nx.json to find proto file in dist
+
+```json
+"targetDefaults": {
+    "build": {
+      "options": {
+        "assets": [
+          {
+            "glob": "*.proto",
+            "input": "proto",
+            "output": "proto"
+          }
+        ]
+      }
+    }
+  }
+
